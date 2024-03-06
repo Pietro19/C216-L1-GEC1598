ada5c542e5c3bf5d379c65e264f7c49e67592e80
Pietro19 <piscardoso15@gmail.com>
2024-03-06 12:07:45 -0300

    [Entrega] Pratica-0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = 'Pietro - 1598'
+function saudacaoPersonalizada(nome) {
+  console.log('Olá, ' + nome + '!')
+}
+saudacaoPersonalizada(nome)
+
+let modulo = require('./module.js')
+modulo.saudacao()
---
+++ pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+  mensagem: 'Testando modulo: Pietro - 1598 !',
+  saudacao: function () {
+    console.log(this.mensagem)
+  }
+}
